7e24b5085fef581160377e08fb4d02e583b498bf
jakehaysumich <112513191+jakehaysumich@users.noreply.github.com>
2022-12-02 12:28:51 -0500

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,9 @@
+document.getElementById("jakebutton").addEventListener("click", function(){
+    alert("Email: "+this.value)
+})
+
+document.getElementById("laurenbutton").addEventListener("click", function(){
+    alert("Email: "+this.value)})
+
+document.getElementById("annabutton").addEventListener("click", function(){
+    alert("Email: "+this.value)})
